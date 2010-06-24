d309f111452d01187ae67542c9fb757fb7f62099
Semmy Purewal <semmy.purewal@gmail.com>
2010-06-24 15:18:53 -0400

    added flickr.feeds module; release 0.1.3

--- src/spotter.flickr.js
+++ src/spotter.flickr.js
@@ -20,14 +20,13 @@ spotter.modules.flickr.search = function(options)  {
     spotter.modules.Module.call(this,options);    
 
     if(options == undefined || options.api_key == undefined || (options.searchString == undefined && options.tags == undefined))
-	throw new Error("flickr module requires an api_key and a searchString or tags to be defined as an option");
+	throw new Error("flickr search module requires an api_key and a searchString or tags to be defined as an option");
 
     var api_key = options.api_key;
     var searchString = options.searchString;
     var tags = options.tags;
 
     var lastTop = {id:-1};  //stupid hack
-
     
     this.url = function()  {
 	var url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search';
@@ -46,6 +45,7 @@ spotter.modules.flickr.search = function(options)  {
 	for(i=0; i < photos.length && photos[i].id !== lastTop.id; i++)  {
 	    photos[i].url = buildPhotoURL(photos[i]);
 	    photos[i].user_url = "http://www.flickr.com/"+photos[i].owner;
+	    photos[i].photo_url = "http://www.flickr.com/"+photos[i].owner+"/"+photos[i].id;
 	    processedData.data.push(photos[i]);
 	}
 
@@ -59,5 +59,40 @@ spotter.modules.flickr.search = function(options)  {
     var buildPhotoURL = function(photo)  {
 	var u = "http://farm" + photo.farm + ".static.flickr.com/"+photo.server+"/"+ photo.id + "_" + photo.secret + ".jpg";
 	return u;
+    }
+}
+
+spotter.modules.flickr.feeds = function(options)  {
+    spotter.modules.Module.call(this,options);
+
+    var tags = options.tags || null;
+
+    var lastTop = {id:-1};  //stupid hack
+
+    this.url = function()  {
+	var url = 'http://api.flickr.com/services/feeds/photos_public.gne?format=json';
+	if(tags !== null) url+= '&tags='+escape(tags);
+	return {url:url, callbackParam:"jsoncallback"};
+    }
+    
+    this.process = function(rawData)  {
+	var processedData = {};
+	processedData.data = [];
+	var photos = rawData.items;
+	var i;
+
+	for(i=0; i < photos.length && photos[i].link !== lastTop.link; i++)  {
+	    photos[i].url = photos[i].media.m.replace("_m","");
+	    photos[i].user_url = "http://www.flickr.com/"+photos[i].author_id;
+	    photos[i].photo_url = photos[i].link;
+	    if(photos[i].author.match(/\(([^\)]*)\)/) === null) alert(photos[i].author);
+	    photos[i].user_id = photos[i].author.match(/\(([^\)]*)\)/)[1];
+	    processedData.data.push(photos[i]);
+	}
+
+	lastTop = photos[0];
+
+	processedData.update = (processedData.data.length>0)?true:false;	
+	return processedData;
     }
 }
